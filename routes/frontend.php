0a595958d46a3184a51e85aff4b51b14eb58d7dc
=hhduong <=hd1171999@gmail.com>
2019-12-26 14:08:54 +0700

    clear code

--- routes/frontend.php
+++
@@ -1,8 +0,0 @@
-<?php 
-use App\Http\Controllers\FrontEnd;
-Route::get('/','IndexController@index');
-Route::get('/{namespace}','IndexController@redirect');
-Route::get('search/product','IndexController@search');
-
-?>
-    
